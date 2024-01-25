8e78a83c839189f3851e8a47b063851b24d7bfd7
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2024-01-24 20:50:16 -0600

    Add ORM methods to language.py

--- lib/models/language.py
+++ lib/models/language.py
@@ -77,3 +77,123 @@ class Language:
             raise ValueError(
                 "classification_id must reference a department in the database"
             )
+
+    @classmethod
+    def create_table(cls):
+        """TODO"""
+        sql = """
+            CREATE TABLE languages(
+                id INTEGER PRIMARY KEY,
+                name VARCHAR(50),
+                number_of_speakers UNSIGNED INT,
+                country_of_origin VARCHAR(50),
+                status TEXT,
+                classification_id INTEGER,
+                FOREIGN KEY (classification_id) REFERENCES classifications(id)
+            );
+        """
+        execute_and_commit(sql)
+
+    @classmethod
+    def drop_table(cls):
+        """TODO may not need to use. Will need to see."""
+        sql = """
+            DROP TABLE IF EXISTS languages
+        """
+
+        execute_and_commit(sql)
+
+    def save(self):
+        """TODO"""
+        sql = """
+            INSERT INTO languages (name, number_of_speakers, country_of_origin, status, classification_id)
+            VALUES (?, ?, ?, ?, ?)
+        """
+
+        execute_and_commit(sql, (self.name, self.number_of_speakers, self.country_of_origin, self.status, self.classification_id))
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    @classmethod
+    def create(cls, name, number_of_speakers, country_of_origin, status, classification_id):
+        """TODO"""
+        language = cls(name, number_of_speakers, country_of_origin, status, classification_id)
+        language.save()
+        return language
+
+    def update(self):
+        """TODO"""
+        sql = """
+            UPDATE languages
+            SET name = ?, number_of_speakers = ?, country_of_origin = ?, status = ?, classification_id = ?,
+            WHERE id = ?
+        """
+        execute_and_commit(sql, (self.name, self.number_of_speakers, self.country_of_origin, self.status, self.classification_id))
+
+    def delete(self):
+        """TODO"""
+
+        sql = """
+            DELETE FROM languages
+            WHERE id = ?
+        """
+
+        execute_and_commit(sql, (self.id,))
+
+        del type(self).all[self.id]
+        self.id = None
+
+    # Review with instructor
+    @classmethod
+    def instance_from_db(cls, row):
+        """TODO"""
+        # Check the language for an existing instance using the row's primary key
+        language = cls.all.get(row[0])
+        if language:
+            # ensure attributes match row values in case local instance was modified
+            language.name = row[1]
+            language.number_of_speakers = row[2]
+            language.country_of_origin = row[3]
+            language.status = row[4]
+            language.classification_id = row[5]
+        else:
+            # not in language, create new instance and add to dictionary
+            language = cls(row[1], row[2], row[3], row[4], row[5])
+        return language
+
+    @classmethod
+    def get_all(cls):
+        """TODO"""
+        sql = """
+            SELECT *
+            FROM languages
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+
+        return [cls.instance_from_db(row) for row in rows]
+
+    @classmethod
+    def find_by_id(cls, id):
+        """TODO"""
+        sql = """
+            SELECT *
+            FROM languages
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
+    @classmethod
+    def find_by_name(cls, name):
+        """TODO"""
+        sql = """
+            SELECT *
+            FROM languages
+            WHERE name is ?
+        """
+
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
